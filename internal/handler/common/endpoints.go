b1dfb633d4b4341becc21c72155ec9fafd631d8a
Vicktor Desrony <vldcreation21@gmail.com>
2025-03-07 09:46:14 +0700

    fix: fix commond endpoint

--- internal/handler/common/endpoints.go
+++ internal/handler/common/endpoints.go
@@ -7,9 +7,9 @@ import (
 
 	goval "github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
-	"github.com/vldcreation/movie-fest/internal/apis"
 	"github.com/vldcreation/movie-fest/internal/apis/common"
 	"github.com/vldcreation/movie-fest/internal/usecase"
+	"github.com/vldcreation/movie-fest/pkg/responsex"
 )
 
 // This is just a test endpoint to get you started. Please delete this endpoint.
@@ -31,19 +31,19 @@ func (s *Server) PostAuthRegister(ctx echo.Context) error {
 	// Parse request body
 	var params common.PostAuthRegisterJSONRequestBody
 	if err := ctx.Bind(&params); err != nil {
-		return ctx.JSON(http.StatusBadRequest, apis.ErrorResponse{Message: err.Error()})
+		return ctx.JSON(http.StatusBadRequest, responsex.ApiError{Message: err.Error()})
 	}
 
 	// Validate request body
 	ctx.Echo().Validator = &Validator{validator: goval.New(goval.WithRequiredStructEnabled())}
 	if err := ctx.Validate(params); err != nil {
-		return ctx.JSON(http.StatusBadRequest, apis.ErrorResponse{Message: err.Error()})
+		return ctx.JSON(http.StatusBadRequest, responsex.ApiError{Message: err.Error()})
 	}
 
 	// Register User
 	res, err := usecase.NewUser(s.cfg, s.tokenMaker, s.repo).Registration(newCtx, params)
 	if err != nil {
-		return ctx.JSON(http.StatusInternalServerError, apis.ErrorResponse{Message: err.Error()})
+		return ctx.JSON(http.StatusInternalServerError, responsex.ApiError{Message: err.Error()})
 	}
 	return ctx.JSON(http.StatusCreated, res)
 }
@@ -55,19 +55,19 @@ func (s *Server) PostAuthLogin(ctx echo.Context) error {
 	// Parse request body
 	var params common.PostAuthLoginJSONRequestBody
 	if err := ctx.Bind(&params); err != nil {
-		return ctx.JSON(http.StatusBadRequest, apis.ErrorResponse{Message: err.Error()})
+		return ctx.JSON(http.StatusBadRequest, responsex.ApiError{Message: err.Error()})
 	}
 
 	// Validate request body
 	ctx.Echo().Validator = &Validator{validator: goval.New(goval.WithRequiredStructEnabled())}
 	if err := ctx.Validate(params); err != nil {
-		return ctx.JSON(http.StatusBadRequest, apis.ErrorResponse{Message: err.Error()})
+		return ctx.JSON(http.StatusBadRequest, responsex.ApiError{Message: err.Error()})
 	}
 
 	// Login User
 	res, err := usecase.NewUser(s.cfg, s.tokenMaker, s.repo).Login(newCtx, params)
 	if err != nil {
-		return ctx.JSON(http.StatusInternalServerError, apis.ErrorResponse{Message: err.Error()})
+		return ctx.JSON(http.StatusInternalServerError, responsex.ApiError{Message: err.Error()})
 	}
 	return ctx.JSON(http.StatusOK, res)
 }
