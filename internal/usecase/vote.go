ffe08c3d1cd28d657a66ec4a3d8ec4802a149789
Vicktor Desrony <vldcreation21@gmail.com>
2025-03-08 14:48:00 +0700

    feat: move vote usecase to user

--- internal/usecase/vote.go
+++ internal/usecase/vote.go
@@ -1,12 +1,7 @@
 package usecase
 
 import (
-	"context"
-	"errors"
-
-	"github.com/google/uuid"
 	"github.com/vldcreation/movie-fest/config"
-	"github.com/vldcreation/movie-fest/consts"
 	"github.com/vldcreation/movie-fest/internal/repository"
 	"github.com/vldcreation/movie-fest/pkg/token"
 )
@@ -23,23 +18,4 @@ func NewVote(cfg *config.Config, tokenMaker token.Maker, repo repository.Reposit
 		repo:       repo,
 		tokenMaker: tokenMaker,
 	}
-}
-
-func (u *User) VoteMovie(ctx context.Context, id uuid.UUID) error {
-	user, ok := ctx.Value(consts.AuthKey).(*token.Payload)
-	if !ok {
-		return errors.New("user not found")
-	}
-
-	userId, ok := user.GetCustomClaims("user_id")
-	if !ok {
-		return errors.New("userIds not found")
-	}
-
-	parseUserId, err := uuid.Parse(userId.(string))
-	if err != nil {
-		return err
-	}
-
-	return u.repo.VoteMovie(ctx, parseUserId, id)
 }
