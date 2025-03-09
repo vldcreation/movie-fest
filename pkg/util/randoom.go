741f09b8d69fdbd28ef5f6d62aa0fdb424b47224
Vicktor Desrony <vldcreation21@gmail.com>
2025-03-09 15:14:14 +0700

    feat: adjust randoom pkg

--- pkg/util/randoom.go
+++ pkg/util/randoom.go
@@ -6,23 +6,36 @@ import (
 )
 
 const (
-	letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	numericBytes  = "0123456789"
+	specialBytes  = "~=+%^*/()[]{}/?>;:<"
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
 )
-
-func init() {
-	println("Seeding random number generator...")
-	rand.Seed(time.Now().UnixNano())
-}
 
 func RandInt(min, max int64) int64 {
 	return min + rand.Int63n(max-min+1)
 }
 
 func RandString(n int) string {
+	return Rand(n, letterBytes)
+}
+
+func Rand(n int, letter string) string {
+	src := rand.NewSource(time.Now().UnixNano())
 	b := make([]byte, n)
-	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+
+	// Use int63() to generate random numbers more efficiently
+	for i, cache, remain := n-1, src.Int63(), letterIdxBits; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxBits
+		}
+		b[i] = letter[int(cache&letterIdxMask)]
+		i--
+		cache >>= letterIdxBits
+		remain--
 	}
+
 	return string(b)
 }
 
@@ -36,4 +49,8 @@ func RandAmount() int64 {
 
 func RandEmail() string {
 	return RandString(6) + "@gmail.com"
+}
+
+func RandSecret(n int) string {
+	return Rand(n, letterBytes+numericBytes+specialBytes)
 }
